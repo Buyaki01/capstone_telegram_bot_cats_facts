39dd171eb151aa893915b62688f2c3df51f829e4
Buyaki01 <rittahbuyaki@gmail.com>
2020-05-26 22:54:28 +0300

    Add cats_facts.rb

---
+++ lib/cats_facts.rb
@@ -0,0 +1,25 @@
+require 'telegram/bot'
+require 'net/http'
+require 'json'
+require_relative 'bot.rb'
+
+class Cats
+    @values = nil
+
+    def initialize
+        @values = make_request_to_api_endpoint
+    end
+
+    def make_request_to_api_endpoint
+        url = 'https://cat-fact.herokuapp.com/facts'
+        uri = URI(url)
+        response = Net::HTTP.get(uri)
+        response = JSON.parse(response)
+        response["all"]
+    end
+
+    def select_random
+        @values = @values.sample
+        @values
+    end
+end
