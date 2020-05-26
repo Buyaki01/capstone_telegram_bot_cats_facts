20f4f4f05a9b8463fb68ca57f555ac79a0f4c6b6
Buyaki01 <rittahbuyaki@gmail.com>
2020-05-26 22:47:56 +0300

    Add main.rb

---
+++ bin/main.rb
@@ -0,0 +1,5 @@
+require_relative '../lib/telegram_bot.rb'
+require_relative '../lib/cats_facts.rb'
+
+puts "Hello, want to know more about cats? Tag along with me to find fascinating facts about cats"
+Bot.new
