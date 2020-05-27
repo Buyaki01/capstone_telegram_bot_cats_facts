52ec46b1d99dea158f4c639c6a38a673c4ffc8a8
Buyaki01 <rittahbuyaki@gmail.com>
2020-05-27 19:28:45 +0300

    Add changes to telegram_bot.rb

--- lib/telegram_bot.rb
+++ lib/telegram_bot.rb
@@ -16,7 +16,7 @@ class Bot
         when '/cats'
           cats = Cats.new
           cat_fact = cats.random_fact_about_cats
-          bot.api.send_message(chat_id: message.chat.id, text: (cat_fact['text']).to_s, date: message.date)
+          bot.api.send_message(chat_id: message.chat.id, text: (cat_fact['text']).to_s)
         else
           bot.api.send_message(chat_id: message.chat.id, text: "#{message.from.first_name}, you need to use  /start,  /stop or /cats")
         end
