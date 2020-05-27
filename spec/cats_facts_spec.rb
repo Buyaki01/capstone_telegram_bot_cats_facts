43ce1c03ba6f240c1096878f9396c708f5d81b2f
Buyaki01 <rittahbuyaki@gmail.com>
2020-05-27 19:44:53 +0300

    Add .rspec and spec in the cats branch

---
+++ spec/cats_facts_spec.rb
@@ -0,0 +1,29 @@
+require_relative '../lib/cats_facts.rb'
+
+RSpec.describe Cats do
+  describe '#make_request_to_api_endpoint' do
+    let(:cats) { Cats.new }
+    let(:make_json_request) { cats_obj.make_request_to_api_endpoint }
+
+    it 'when the request is sucessful should return Json response' do
+      expect(make_json_request.class).to eql(Array)
+    end
+
+    it 'the array should not be empty' do
+      expect(make_json_request.length).not_to eql(0)
+    end
+  end
+
+  describe '#random_fact_about_cats' do
+    let(:cats) { Cats.new }
+    let(:call_method) { cats.select_random }
+
+    it 'return a Hash' do
+      expect(call_method.class).to eql(Hash)
+    end
+
+    it 'return six items in a hash' do
+      expect(call_method.length).to eql(6)
+    end
+  end
+end
