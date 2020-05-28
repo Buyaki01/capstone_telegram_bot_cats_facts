7cf97ad8de68568f2ebc20c0560a09968a4fbe3a
Buyaki01 <rittahbuyaki@gmail.com>
2020-05-28 19:55:30 +0300

    Fix errors in cats_facts_spec.rb

--- spec/cats_facts_spec.rb
+++ spec/cats_facts_spec.rb
@@ -3,7 +3,7 @@ require_relative '../lib/cats_facts.rb'
 RSpec.describe Cats do
   describe '#make_request_to_api_endpoint' do
     let(:cats) { Cats.new }
-    let(:make_json_request) { cats_obj.make_request_to_api_endpoint }
+    let(:make_json_request) { cats.make_request_to_api_endpoint }
 
     it 'when the request is sucessful should return Json response' do
       expect(make_json_request.class).to eql(Array)
@@ -16,7 +16,7 @@ RSpec.describe Cats do
 
   describe '#random_fact_about_cats' do
     let(:cats) { Cats.new }
-    let(:call_method) { cats.select_random }
+    let(:call_method) { cats.random_fact_about_cats }
 
     it 'return a Hash' do
       expect(call_method.class).to eql(Hash)
